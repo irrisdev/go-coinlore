a1fbd0821a46e65a07e6ac87f60ea9a7939023d1
Irris <160921248+irrisdev@users.noreply.github.com>
2024-11-20 20:09:38 +0000

    Update main.go

--- main.go
+++ main.go
@@ -8,20 +8,20 @@ import (
 )
 
 func main() {
-	
-    client := coinlore.NewClient("https://api.coinlore.net")
 
-    // Get global data
-    global, err := client.GetGlobal()
-    if err != nil {
-        log.Fatalf("Error fetching global data: %v", err)
-    }
-    fmt.Printf("Global Coins Count: %d\n", global.CoinsCount)
+	client := coinlore.NewClient("https://api.coinlore.net")
 
-    // Get coin data
-    coin, err := client.GetCoin(905454)
-    if err != nil {
-        log.Fatalf("Error fetching coin data: %v", err)
-    }
-    fmt.Printf("Coin: %s, Price: %s USD\n", coin.Name, coin.PriceUSD)
+	// Get global data
+	global, err := client.GetGlobal()
+	if err != nil {
+		log.Fatalf("Error fetching global data: %v", err)
+	}
+	fmt.Printf("Global Coins Count: %d\n", global.CoinsCount)
+
+	// Get coin data
+	coin, err := client.GetCoin(905454)
+	if err != nil {
+		log.Fatalf("Error fetching coin data: %v", err)
+	}
+	fmt.Printf("Coin: %s, Price: %s USD\n", coin.Name, coin.PriceUSD)
 }
